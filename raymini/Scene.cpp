6e67ed4bed13253d8e6cb8e4903012c160a45fd6
Daniel Ross <daniel.je.ross@gmail.com>
2011-04-20 19:47:10 +0200

    start adding radius for area light need to add parameter in Light RayTracer

--- raymini/Scene.cpp
+++ raymini/Scene.cpp
@@ -55,6 +55,7 @@ void Scene::buildDefaultScene (bool HD) {
     Mesh ramMesh;
     if (HD)
         ramMesh.loadOFF ("models/ram_HD.off");
+        //ramMesh.loadOFF ("models/max_50k.off");
     else
         ramMesh.loadOFF ("models/ram.off");
     Material ramMat (1.f, 1.f, 128.f, Vec3Df (1.f, .6f, .2f));
