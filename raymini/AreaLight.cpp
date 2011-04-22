a2031b009fe75eb050db23ee1b887f4bfb063da3
Daniel Ross <daniel.je.ross@gmail.com>
2011-04-22 15:59:43 +0200

    small changes and commit to pull arnaud's branch

--- raymini/AreaLight.cpp
+++ raymini/AreaLight.cpp
@@ -10,17 +10,23 @@ void AreaLight::setDirection(const Vec3Df & dir) {
     v.normalize();
 }
 
+// rand() is pseudo random. Does it matter?
+// Just means we will compute the same soft shadows each time!
 float AreaLight::getVisibility(Vec3Df & point, Vec3Df & dir, std::vector<Object> & objects)
 {
     int visibility = 0;
-    for(int i=0; i<resolution; ++i)
+    for(unsigned int i=0; i<resolution; ++i)
     {
-        float angle = (float) (rand() % 10000) * 2.f * M_PI/ 10000.f;
-        float r     = (float) (rand() % 10000) * radius * M_PI/ 10000.f;
+        float angle = (float) (rand()/(float)RAND_MAX) * 2.f * M_PI;
+        //float r     = (float) (rand() % 10000) * radius * M_PI/ 10000.f;
+        float r = (float) (rand()/(float)RAND_MAX) * radius;
         Vec3Df pos  = getPos() + r * (cos(angle)*u + sin(angle)*v);
+        dir = pos - point;
+        dir.normalize();
 
         if(isVisible(point, dir, objects))
             visibility++;
     }
-	return (float) visibility / (float) resolution;
+    return 0.5f;
+	//return (float) visibility / (float) resolution;
 }
